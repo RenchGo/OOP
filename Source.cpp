23c06d70366d875b97331ee59e5be55384d09514
RenchGo <94788714+RenchGo@users.noreply.github.com>
2021-12-17 12:53:38 +0300

    Lab7

--- Source.cpp
+++ Source.cpp
@@ -1,86 +1,238 @@
 #include <iostream>
 using namespace std;
 
-void ff() {
-	cout << "Глобальынй уровень" << endl;
+template <class T> void swap(const T& a, const T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
 }
-class BASE {
-public: int i;
-	  BASE(int _i, long _l, double _d) {
-		  i = _i;
-		  l = _l;
-		  d = _d;
-	  }
-	  BASE() {
-		  i = 11;
-		  l = 22;
-		  d = 33;
-		  cout << "Был вызван конструтор по умолчанию из класса BASE" << endl;
+
+template <typename T> T max(T& a, T& b)
+{
+    cout << "template" << endl;
+    return a > b ? a : b;
 }
-	  void show() {
-		  cout << "i занимает " << sizeof(i) << " байт(а), значение = " << i << endl;
-		  cout << "l занимает " << sizeof(l) << " байт(а), значение = " << l << endl;
-		  cout << "d занимает " << sizeof(d) << " байт(а), значение = " << d << endl;
-	  }
-	  void ff() {
-		  cout << "Класс Base" << endl;
-	  }
-private: long l;
-protected: double d;
-friend class DERIVED;
+
+const char* max(const char* a, const char* b)
+{
+    cout << "specialized" << endl;
+    return (a > b) ? a : b;
+}
+class Complex {
+private:
+
+    double m_re, m_im;
+
+public:
+
+    Complex();
+    Complex(double re, double im = 0);
+
+    double GetRe() {
+        return m_re;
+    }
+    double GetIm() {
+        return m_im;
+    }
+
+
+    Complex operator*(const Complex& a) {
+        return Complex(m_re * a.m_re - m_im * a.m_im,
+            m_re * a.m_im - m_im * a.m_re);
+    }
+
+    Complex operator*(const double& a) {
+        return Complex(m_re * a, m_im * a);
+    }
+
+    Complex& operator=(const Complex& a)
+    {
+        if (this != &a)
+        {
+            m_re = a.m_re;
+            m_im = a.m_im;
+        }
+        return *this;
+    }
+    double& operator=(const double& a)
+    {
+        return m_re;
+    }
+
+    friend ostream& operator<< (ostream& out, const Complex& x)
+    {
+        return (out << "(" << x.m_re << "," << x.m_im << ")");
+    }
 };
 
-class DERIVED: public BASE {
+
+template <class T>
+class matrix {
+
+private:
+    T** M;
+    int m;
+    int n;
+
 public:
-	DERIVED(float _f, int _i, long _l, double _d ){
-		f = _f;
-		i = _i;
-		l = _l;
-		d = _d;
-	}
-	DERIVED() {
-		f =44;
-	}
-	void show() {
-		cout << "i занимает " << sizeof(i) << " байт(а), значение = " << i << endl;
-		cout << "l занимает " << sizeof(l) << " байт(а), значение = " << l << endl;
-		cout << "d занимает " << sizeof(d) << " байт(а), значение = " << d << endl;
-		cout << "f занимает " << sizeof(f) << " байт(а), значение = " << f << endl;
-	}
-	void ff() {
-		cout << "Класс DERIVED" << endl;
-	}
-private: float f;
- friend class DERIVED_1;
+
+    matrix()
+    {
+        n = m = 0;
+    }
+
+    matrix(int _m, int _n)
+    {
+        m = _m;
+        n = _n;
+
+        M = (T**) new T * [m];
+
+
+        for (int i = 0; i < m; i++)
+            M[i] = (T*)new T[n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                M[i][j] = 0;
+    }
+
+    void SetMij(int i, int j, T value)
+    {
+        if ((i < 0) || (i >= m))
+            return;
+        if ((j < 0) || (j >= n))
+            return;
+        M[i][j] = value;
+    }
+
+    void Print(const char* ObjName)
+    {
+        cout << "Object: " << ObjName << endl;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+                cout << M[i][j] << "\t";
+            cout << endl;
+        }
+        cout << "---------------------" << endl << endl;
+    }
+
+    matrix operator=(const matrix& _M)
+    {
+        if (n > 0)
+        {
+
+            for (int i = 0; i < m; i++)
+                delete[] M[i];
+        }
+
+        if (m > 0)
+        {
+            delete[] M;
+        }
+
+        m = _M.m;
+        n = _M.n;
+
+        M = (T**) new T * [m];
+        for (int i = 0; i < m; i++)
+            M[i] = (T*) new T[n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                M[i][j] = _M.M[i][j];
+        return *this;
+
+
+    }
+    ~matrix()
+    {
+        if (n > 0)
+        {
+            for (int i = 0; i < m; i++)
+                delete[] M[i];
+        }
+
+        if (m > 0)
+            delete[] M;
+    }
+
+
 };
 
-class DERIVED_1 : public DERIVED {
+template <typename T> class Stack
+{
+    T* stackPtr;
+    int size;
+    T top;
+
 public:
-	void foo() {
-		ff();
-	}
-	void ff() {
-		cout << "Класс DERIVED_1" << endl;
-	}
-	  /*DERIVED_1(float _f, int _i, long _l, double _d) {
-		  f = _f;
-		  i = _i;
-		  l = _l;
-		  d = _d;
-	  }*/
+    Stack(int = 10);
+
+    ~Stack();
+
+    bool push(const T);
+    bool pop();
+    void printStack();
 };
+
+template <typename T> Stack<T>::Stack(int s)
+{
+    size = s > 0 ? s : 10;
+    stackPtr = new T[size];
+    top = -1;
+}
+
+template <typename T> Stack<T>::~Stack()
+{
+    delete[] stackPtr;
+}
+
+template <typename T> bool Stack<T>::push(const T value)
+{
+    if (top == size - 1)
+        return false;
+
+    top++;
+    stackPtr[top] = value;
+
+    return true;
+}
+
+template <typename T> bool Stack<T>::pop()
+{
+    if (top == -1)
+        return false;
+
+    stackPtr[top] = 0;
+    top--;
+
+    return true;
+}
+
+template <typename T> void Stack<T>::printStack()
+{
+    for (int ix = size - 1; ix >= 0; ix--)
+        cout << "|" << setw(4) << stackPtr[ix] << endl;
+}
 
 int main() {
-	setlocale(LC_ALL, "rus");
-	BASE Obj1(66,77,88);
-	DERIVED Obj2(99,111,222,333);
-	DERIVED_1 Obj3;
-	cout <<"Размер BASE = "<<sizeof(BASE)<<endl;
-	Obj1.show();
-	cout << "Размер DERIVED = "<<sizeof(DERIVED)<< endl;
-	Obj2.show();
-	cout << "Размер DERIVED_1 = " << sizeof(DERIVED_1) << endl;
-	Obj3.show();
-	Obj3.foo();
-	return 0;
-};
+
+    int i = max(4, 8);
+    double d = max(17.2, 17.3);
+    const char* ch = max("Hello", "World");
+    cout << "i = " << i << endl;
+    cout << "d = " << d << endl;
+    cout << "ch = " << ch << endl;
+
+    matrix<int> in(2, 3);
+    in.SetMij(1, 1, 2.1);
+    in.SetMij(1, 2, 3);
+    in.SetMij(2, 1, 'h');
+
+    Complex comp(1.0);
+
+    in.SetMij(2, 2, comp);
+    return 0;
+}
